205d81b4cbfab6098dcc2b8fd9625457033e0713
Oscar Miranda <omiranda0058@gmail.com>
2019-09-22 19:56:28 -0500

    added to orm.js and connection.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgersController.js");
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
