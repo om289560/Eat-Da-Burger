6195d6f52a0af25592180fc6caf7b00ca44dd6b4
Oscar Miranda <omiranda0058@gmail.com>
2019-09-22 20:14:10 -0500

    fixed issues with burger.js and burgersController.js

--- models/burger.js
+++ models/burger.js
@@ -1,52 +1,28 @@
-var express = require("express");
-var burger = require("../models/burger");
-
-var router = express.Router();
-
-router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
-    var hdbrsObj = {
-      burgers: data
-    };
-    console.log(hdbrsObj);
-    res.render("index", hdbrsObj);
-  });
-
-  router.post("/api/burgers", function(req, res) {
-    burger.insertOne(
-      ["burger_name", "devoured"],
-      [req.body.burger_name, req.body.devoured],
-      function(result) {
-        // Send back the ID of new burger
-        res.json({ id: result.insertId });
-      }
-    );
-  });
-  router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-
-    console.log("condition", condition);
-    burger.updateOne({ devoured: req.body.devoured }, condition, function(
-      result
-    ) {
-      if (result.changedRows === 0) {
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
+var orm = require("../config/orm.js");
+var burger = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
     });
-  });
-  router.delete("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-    console.log("condition", condition);
+  },
 
-    burger.deleteOne(condition, function(result) {
-      if (result.changedRows === 0) {
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
     });
-  });
-});
-module.exports = router;
+  },
+
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+
+  deleteOne: function(condition, cb) {
+    orm.deleteOne("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
